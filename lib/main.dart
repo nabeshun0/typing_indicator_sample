509e0477ec81a1019a5dc0443091e25cee61743e
snt-nbzw <sntnbzw@gmail.com>
2021-03-08 23:52:57 +0900

    Improve: make room for the typing indicator

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,12 @@ class _ExampleIsTypingPageState extends State<ExampleIsTypingPage> {
           Expanded(
             child: _buildMessages(),
           ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: TypingIndicator(
+              showIndicator: _isSomeoneTyping,
+            ),
+          ),
           _buildIsTypingSimulator()
         ],
       ),
@@ -87,6 +93,96 @@ class FakeMessages extends StatelessWidget {
       decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(8)),
           color: Colors.grey.shade300),
+    );
+  }
+}
+
+class TypingIndicator extends StatefulWidget {
+  const TypingIndicator({
+    Key? key,
+    this.showIndicator = false,
+    this.bubbleColor = const Color(0xFF646b7f),
+    this.flashingCircleDarkColor = const Color(0xFF333333),
+    this.flashingCircleBrightColor = const Color(0xFFaec1dd),
+  }) : super(key: key);
+
+  final bool showIndicator;
+  final Color bubbleColor;
+  final Color flashingCircleDarkColor;
+  final Color flashingCircleBrightColor;
+
+  @override
+  _TypingIndicatorState createState() => _TypingIndicatorState();
+}
+
+class _TypingIndicatorState extends State<TypingIndicator>
+    with TickerProviderStateMixin {
+  late AnimationController _appearanceController;
+  late Animation<double> _indicatorSpaceAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _appearanceController = AnimationController(
+      vsync: this,
+    );
+
+    _indicatorSpaceAnimation = CurvedAnimation(
+      parent: _appearanceController,
+      curve: const Interval(0.0, 0.4, curve: Curves.easeOut),
+      reverseCurve: const Interval(0.0, 1.0, curve: Curves.easeOut),
+    ).drive(Tween<double>(
+      begin: 0.0,
+      end: 60.0,
+    ));
+
+    if (widget.showIndicator) {
+      _showIndicator();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant TypingIndicator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.showIndicator != oldWidget.showIndicator) {
+      if (widget.showIndicator) {
+        _showIndicator();
+      } else {
+        _hideIndicator();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _appearanceController.dispose();
+    super.dispose();
+  }
+
+  void _showIndicator() {
+    _appearanceController
+      ..duration = const Duration(milliseconds: 750)
+      ..forward();
+  }
+
+  void _hideIndicator() {
+    _appearanceController
+      ..duration = const Duration(milliseconds: 150)
+      ..reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO:
+    return AnimatedBuilder(
+      animation: _indicatorSpaceAnimation,
+      builder: (context, child) {
+        return SizedBox(
+          height: _indicatorSpaceAnimation.value,
+        );
+      },
     );
   }
 }
